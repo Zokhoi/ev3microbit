6a6270dbfe71712452e1e9ec3a3599df45a64cff
Zokhoi <20432565+Zokhoi@users.noreply.github.com>
2022-06-09 21:47:20 +0800

    added logic
    Co-authored-by: Notstevee <44204297+Notstevee@users.noreply.github.com>

--- ev3/main.py
+++ ev3/main.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
 
-# from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-# from ev3dev2.sensor import INPUT_1
-# from ev3dev2.sensor.lego import TouchSensor
-# from ev3dev2.led import Leds
-
-import pygatt
 from time import sleep
-import binascii
 import json
-
-adapter = pygatt.GATTToolBackend()
-
-DELAY = 1.0
 
 f = open('config.json')
 config = json.load(f)
@@ -20,31 +9,66 @@ f.close()
 
 MACADDR = config['MACADDR']
 
+from ev3dev2.sensor import INPUT_1,INPUT_2
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.stopwatch import StopWatch
+
+import pygatt
+print("import successful!")
+
+adapter = pygatt.GATTToolBackend()
+
 try:
-  adapter.start()
+    adapter.start()
+    device = adapter.connect(MACADDR, address_type=pygatt.BLEAddressType.random)
+    print("Bluetooth connection successful!")
+except:
+    print("Bluetooth connection failed!")
 
-  print("Trying to connect to " + MACADDR + " ...")
-  device = adapter.connect(MACADDR, address_type=pygatt.BLEAddressType.random)
-  print('Connected to ' + MACADDR)
-  for uuid in device.discover_characteristics().keys():
-    print("Read UUID %s: %s" % (uuid, binascii.hexlify(device.char_read(uuid))))
+buttonA = device.char_read("e95dda90-251d-470a-a062-fa1922dfa9a8")
+buttonB = device.char_read("e95dda91-251d-470a-a062-fa1922dfa9a8")
 
-  # while True:
-  #   device.char_write_handle(0x2E, bytearray([0x1F, 0x11, 0x11, 0x11, 0x1F]))
-  #   sleep(DELAY)
-  #   device.char_write_handle(0x2E, bytearray([0x00, 0x0E, 0x0A, 0x0E, 0x00]))
-  #   sleep(DELAY)
-  #   device.char_write_handle(0x2E, bytearray([0x00, 0x00, 0x04, 0x00, 0x00]))
-  #   sleep(DELAY)
-  #   device.char_write_handle(0x2E, bytearray([0x00, 0x00, 0x00, 0x00, 0x00]))
-  #   sleep(DELAY)
-  #   device.char_write_handle(0x2E, bytearray([0x11, 0x0A, 0x04, 0x0A, 0x11]))
-  #   sleep(DELAY)
-  #   device.char_write_handle(0x2E, bytearray([0x00, 0x0A, 0x04, 0x0A, 0x00]))
-  #   sleep(DELAY)
-  #   device.char_write_handle(0x2E, bytearray([0x00, 0x00, 0x04, 0x00, 0x00]))
-  #   sleep(DELAY)
-  #   device.char_write_handle(0x2E, bytearray([0x00, 0x00, 0x00, 0x00, 0x00]))
-  #   sleep(DELAY)
-finally:
-  adapter.stop()
+numbytearray=[bytearray([0x06,0x09,0x09,0x09,0x16]),bytearray([0x04,0x0C,0x04,0x04,0x1F]),bytearray([0x0C,0x12,0x04,0x08,0x1F]),bytearray([0x0E,0x02,0x0E,0x02,0x0E]),bytearray([0x14,0x14,0x1E,0x04,0x04]),bytearray([0x0E,0x08,0x0E,0x01,0x0E]),bytearray([0x06,0x08,0x1E,0x11,0x0E]),bytearray([0x0F,0x02,0x04,0x04,0x08]),bytearray([0x0E,0x11,0x0E,0x11,0x0E]),bytearray([0x0E,0x11,0x0E,0x02,0x02])]
+
+start_touch=TouchSensor(INPUT_1)
+end_touch=TouchSensor(INPUT_2)
+timer=StopWatch
+while True:
+    
+    while True:
+        
+        if start_touch.is_pressed():
+            timer.start()
+        
+        if end_touch.is_pressed():
+            timer.stop()
+            time=timer.value_secs()
+            break
+
+
+        sleep(0.1)
+
+    delay=0.6
+    time=str(time)
+    for p in range(3):
+        for i in time:
+            if i==".":
+                device.char_write_handle(0x27, bytearray([0x00,0x00,0x00,0x0C,0x0C]))
+                sleep(delay)
+            else:
+                device.char_write_handle(0x27, numbytearray[int(i)])
+                sleep(delay)
+            device.char_write_handle(0x27, bytearray([0x0E,0x10,0x0E,0x01,0x0E]))
+            sleep(delay*3)
+
+    while True:
+        if buttonA == bytearray(b'\x01'):
+            timer.reset()
+            break
+        elif buttonB == bytearray(b'\x01'):
+            timer.reset()
+            break
+
+        sleep(0.1)
+    
+    
